19542a8dee23d7dcd61cc1d7d17cca050ac5191f
root <drawkend@gmail.com>
2023-03-08 09:02:52 -0500

    TP4.3

--- TP4/maxmin.c
+++ TP4/maxmin.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 
+void triSelection(int tableau[], int taille) {
+    int i, j, min, tmp;
+    for (i = 0; i < taille - 1; i++) {
+        min = i;
+        for (j = i + 1; j < taille; j++) {
+            if (tableau[j] < tableau[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            tmp = tableau[i];
+            tableau[i] = tableau[min];
+            tableau[min] = tmp;
+        }
+    }
+}
+
+
+
+
+
 int main() {
     int n, i, min, max, sum = 0;
-    printf("Combien de nombres voulez vous donner ? ");
+    printf("Combien de nombres voulez-vous donner ? ");
     scanf("%d", &n);
     int tab[n];
+    int tabsort[n];
     for (i = 0; i < n; i++) {
-        printf("Entrez le nombre numero  %d : ", i + 1);
+        printf("Entrez le nombre %d : ", i + 1);
         scanf("%d", &tab[i]);
         sum += tab[i];
         if (i == 0) {
@@ -20,12 +42,18 @@ int main() {
             }
         }
     }
-    printf("Voici les nombres contenus dans le tableau : ");
+    printf("Les nombres que vous avez entrés sont : ");
     for (i = 0; i < n; i++) {
         printf("%d ", tab[i]);
     }
+
+    printf("\n");
+
+    triSelection(tab,n);
     printf("\nLa somme de ces nombres est : %d\n", sum);
-    printf("Le minimum est : %d\n", min);
-    printf("Le maximum est : %d\n", max);
+    printf("Le minimum est : %d\n", tab[0]);
+    printf("Le maximum est : %d\n", tab[n-1]);
     return 0;
 }
+
+ 
